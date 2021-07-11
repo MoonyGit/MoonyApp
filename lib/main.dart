6a3d8e32fa1b34965eec54da8d11b8140e594525
Sebastien Delaherche <sebastien.delaherche@renault.com>
2021-07-11 21:23:10 +0200

    chore: add firebase to the app
    Refs: https://trello.com/c/82rtMAUF

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:moony_app/common/application/app_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MoonyApp());
 }
