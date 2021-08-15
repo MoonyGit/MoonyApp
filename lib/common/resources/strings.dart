12083ebe6ace994d1253c8ebbcfc37604a84c709
Alexandre MAINDRON <amaindron.moony@gmail.com>
2021-08-15 19:06:48 +0200

    refactor: strings by categories

--- lib/common/resources/strings.dart
+++ lib/common/resources/strings.dart
@@ -3,8 +3,48 @@ import 'package:get/get.dart';
 /// This class is used to provide all string constant in a statical way
 class AppStrings {
 
+  ///////////////////////////
+  ///////// Global /////////
+  //////////////////////////
+
   /// The application name
   static const String appTitle = "Moony Application";
+
+  /// Global next
+  static const String goToNext = "Next";
+
+  /// Global continue
+  static const String goToContinue = "Continuer";
+
+  /// No matter text
+  static const String noMatter = "Peu importe";
+
+  /// Man text
+  static const String genderMan = "Homme";
+
+  /// Woman text
+  static const String genderWoman = "Femme";
+
+  /// Couple text
+  static const String relationshipCouple = "Couple";
+
+  /// Single text
+  static const String relationshipSingle = "Célibataire";
+
+  /// Hetero text
+  static const String hetero = "Hetero";
+
+  /// Homo text
+  static const String homosexual = "Homo";
+
+  /// Method to translate strings
+  static String translate({required String message}) {
+    return message.tr;
+  }
+
+  ///////////////////////////
+  ////////// Login //////////
+  //////////////////////////
 
   /// The generic string for authentication failures
   static const String genericAuthFailure = "Something went wrong";
@@ -51,9 +91,6 @@ class AppStrings {
   /// Apple login button text
   static const String loginWithApple = "Connection with Apple";
 
-  /// Global next
-  static const String goToNext = "Next";
-
   /// Phone number hint
   static const String setPhoneNumberHint =
       "AYO! I am Moony! \nI will send you a SMS with a verification code";
@@ -61,8 +98,56 @@ class AppStrings {
   /// Phone otp hint
   static const String smsOtpHint = "ENVOYE! \n Ton code est \n...";
 
-  /// Method to translate strings
-  static String translate({required String message}) {
-    return message.tr;
-  }
+  ///////////////////////////
+  //// Activity Creation ////
+  ///////////////////////////
+
+  /// Activity type text on activity creation process
+  static const String activityTypeTitle = "Type d'activité";
+
+  /// Multiple selection text on activity creation process
+  static const String activityFilterTitle = "FILTRE PARTICIPANTS";
+
+  /// Multiple selection text on activity creation process
+  static const String activityFilterSubTitle = "(sélection multiple)";
+
+  /// Travel text on activity creation process
+  static const String activityTypeTravel = "Voyage";
+
+  /// Sport text on activity creation process
+  static const String activityTypeSport = "Sport";
+
+  /// Food text on activity creation process
+  static const String activityTypeFood = "Bar & Food";
+
+  /// Culture text on activity creation process
+  static const String activityTypeCulture = "Loisir & Culture";
+
+  /// Participants number text on activity creation process
+  static const String activityParticipantNumber = "Nombre d'invités";
+
+  /// 1 Participant text on activity creation process
+  static const String activityNumber1 = "1 (Binôme)";
+
+  /// 2 Participants text on activity creation process
+  static const String activityNumber2 = "2 (Trio)";
+
+  /// 3 Participants text on activity creation process
+  static const String activityNumber3 = "3 (Quatuor)";
+
+  /// Activity creation process: Budget title
+  static const String activityBudgetTitle = "Budget";
+
+  /// Activity creation process: Budget= Free
+  static const String activityBudgetFree = "Gratuit";
+
+  /// Activity creation process: Budget= Free
+  static const String activityBudgetLv1 = "1€ - 20€";
+
+  /// Activity creation process: Budget= Free
+  static const String activityBudgetLv2 = "20€ - 50€";
+
+  /// Activity creation process: Budget= Free
+  static const String activityBudgetLv3 = "+50€";
+
 }
