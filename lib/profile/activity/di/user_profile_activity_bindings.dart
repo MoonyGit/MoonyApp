980a4fd5789ec57b742eb43b4648aa03534ec15f
Sebastien Delaherche <sebastien.delaherche@gmail.com>
2022-02-21 18:10:30 +0100

    fix: use better mock

--- lib/profile/activity/di/user_profile_activity_bindings.dart
+++ lib/profile/activity/di/user_profile_activity_bindings.dart
@@ -35,7 +35,7 @@ class UserProfileActivityBinding extends Bindings {
         {
           /// Data source
           Get.lazyPut<UserProfileActivitiesDataSource>(
-                () => MockService(),
+                () => Get.find<MockService>(),
           );
           break;
         }
