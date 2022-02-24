79a01fb07406dcb6653a5c3add425e3ac004b129
Jordane Serreau <jordaneserreau@gmail.com>
2022-02-24 21:34:14 +0100

    feat(user-profile-activity): add presentation layer

--- lib/profile/resources/strings.dart
+++ lib/profile/resources/strings.dart
@@ -8,3 +8,26 @@ const String userProfileFroonys = "Froonys";
 
 /// user profile banner activity number title
 const String userProfileActivity = "Activities";
+
+/// User profile activity screen text
+
+/// user profile number of activity header
+const String userProfileActivityComing = "To come up";
+
+/// user profile create activity button if list is empty
+const String userProfileActivityCreate = "Create an activity";
+
+/// user profile no activity description text
+const String userProfileActivityNoContent = "Nothing planned soon ?";
+
+/// user profile activity item button select
+const String userProfileActivityItemSelectable = "Select";
+
+/// user profile activity waiting partner
+const String userProfileActivityPartnerWaiting = "persons waiting";
+
+/// user profile activity delay
+const String userProfileActivityDelay = "day(s) remaining";
+
+/// User profile settings button
+const String userProfileSetting = "Setting";
