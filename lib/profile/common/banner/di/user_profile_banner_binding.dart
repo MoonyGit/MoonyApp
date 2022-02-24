9a8faab23b8f270d412c0d7fa0f731e23a398deb
Jordane Serreau <jordaneserreau@gmail.com>
2022-02-24 20:34:32 +0100

    feat(user-profile-banner): add di layer

---
+++ lib/profile/common/banner/di/user_profile_banner_binding.dart
@@ -0,0 +1,46 @@
+import 'package:get/get.dart';
+import 'package:moony_app/common/data/mock/mock_service.dart';
+import 'package:moony_app/flavors.dart';
+import 'package:moony_app/profile/common/banner/data/repository/user_profile_banner_repository_impl.dart';
+import 'package:moony_app/profile/common/banner/data/source/user_profile_banner_data_source.dart';
+import 'package:moony_app/profile/common/banner/domain/repository/user_profile_banner_repository.dart';
+import 'package:moony_app/profile/common/banner/domain/usecase/get_user_profile_banner_usecase.dart';
+import 'package:moony_app/profile/common/banner/presentation/controller/user_profile_banner_controller.dart';
+
+/// Bindings for user profile banner
+class UserProfileBannerBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(
+          () => UserProfileBannerController(
+        Get.find<GetUserProfileBannerUseCase>(),
+      ),
+    );
+
+    /// UseCase
+    Get.lazyPut(() => GetUserProfileBannerUseCase(Get.find()));
+
+    /// Repository
+    Get.lazyPut<UserProfileBannerRepository>(
+          () => UserProfileBannerRepositoryImpl(Get.find()),
+    );
+
+    switch (F.appFlavor) {
+      case Flavor.MOCK:
+        {
+          /// Data source
+          Get.lazyPut<UserProfileBannerDataSource>(
+                () => MockService(),
+          );
+          break;
+        }
+      default:
+        {
+          /// Data UserProfileBannerDataSource
+          Get.lazyPut<UserProfileBannerDataSource>(
+                () => UserProfileBannerDataSourceImpl(),
+          );
+        }
+    }
+  }
+}
