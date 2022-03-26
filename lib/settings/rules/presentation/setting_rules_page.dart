26342a9518903ccd4551e54bfe366eb9b4db975f
Jordroid <jordaneserreau@gmail.com>
2022-03-26 15:23:15 +0100

    feat: add presenter layer for settings rules

--- lib/settings/rules/presentation/setting_rules_page.dart
+++ lib/settings/rules/presentation/setting_rules_page.dart
@@ -1,13 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:moony_app/settings/profile/presentation/setting_profile_controller.dart';
+import 'package:moony_app/common/resources/strings.dart';
+import 'package:moony_app/common/resources/themes.dart';
+import 'package:moony_app/settings/common/presentation/setting_header_widget.dart';
+import 'package:moony_app/settings/common/presentation/setting_item_widget.dart';
+import 'package:moony_app/settings/resources/strings.dart';
 import 'package:moony_app/settings/rules/presentation/setting_rules_controller.dart';
 
 /// The setting rules page
 class SettingRulesPage extends GetView<SettingRulesController> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return const Scaffold();
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: <Color>[AppTheme.secondary, AppTheme.primary],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+              size: 24,
+            ),
+            onPressed: () => controller.back(),
+          ),
+          title: Text(
+            settingRuleTitle.translate(),
+            style: const TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: <Widget>[
+            SettingHeaderWidget(
+              title: settingRuleHeaderLegal.translate().toUpperCase(),
+            ),
+            SettingItemWidget(
+              title: settingRulePrivacyPolicy.translate(),
+              listener: controller.redirectToPrivacy,
+            ),
+            SettingItemWidget(
+              title: settingRuleTermOfUse.translate(),
+              listener: controller.redirectToTerm,
+            ),
+            SettingItemWidget(
+              title: settingRuleLicences.translate(),
+              listener: controller.redirectToLicences,
+            ),
+            SettingItemWidget(
+              title: settingRuleSettlement.translate(),
+              listener: controller.redirectToSettlement,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
